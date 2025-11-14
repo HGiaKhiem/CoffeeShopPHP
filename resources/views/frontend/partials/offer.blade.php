4f85bec655a9bf4c89cd3d138c5366ee399c525a
Duy Nguyen <nduy4085@gmail.com>
2025-11-15 00:24:03 +0700

    Update
    Update giao diện

--- resources/views/frontend/partials/offer.blade.php
+++ resources/views/frontend/partials/offer.blade.php
@@ -2,13 +2,13 @@
 <div class="offer container-fluid my-5 py-5 text-center position-relative overlay-top overlay-bottom">
     <div class="container py-5">
         <h1 class="display-3 text-primary mt-3">50% OFF</h1>
-        <h1 class="text-white mb-3">Sunday Special Offer</h1>
-        <h4 class="text-white font-weight-normal mb-4 pb-3">Only for Sunday from 1st Jan to 30th Jan 2045</h4>
+        <h1 class="text-white mb-3">Ưu Đãi Đặc Biệt Chủ Nhật</h1>
+        <h4 class="text-white font-weight-normal mb-4 pb-3">Chỉ áp dụng vào Chủ Nhật từ ngày 01/11/2025 đến 30/11/2025</h4>
         <form class="form-inline justify-content-center mb-4">
             <div class="input-group">
-                <input type="text" class="form-control p-4" placeholder="Your Email" style="height: 60px;">
+                <input type="text" class="form-control p-4" placeholder="Email của bạn" style="height: 60px;">
                 <div class="input-group-append">
-                    <button class="btn btn-primary font-weight-bold px-4" type="submit">Sign Up</button>
+                    <button class="btn btn-primary font-weight-bold px-4" type="submit">Đăng Ký</button>
                 </div>
             </div>
         </form>
--- resources/views/frontend/partials/services.blade.php
+++ resources/views/frontend/partials/services.blade.php
@@ -2,23 +2,54 @@
 <div class="container-fluid pt-5">
     <div class="container">
         <div class="section-title">
-            <h4 class="text-primary text-uppercase" style="letter-spacing: 5px;">Our Services</h4>
-            <h1 class="display-4">Fresh & Organic Beans</h1>
+            <h4 class="text-primary text-uppercase" style="letter-spacing: 5px;">OUR SERVICES</h4>
+            <h1 class="display-4">Hạt Cà Phê Tươi & Hữu Cơ</h1>
         </div>
         <div class="row">
-            @for ($i = 1; $i <= 4; $i++)
             <div class="col-lg-6 mb-5">
                 <div class="row align-items-center">
                     <div class="col-sm-5">
-                        <img class="img-fluid mb-3 mb-sm-0" src="{{ asset('img/service-' . $i . '.jpg') }}" alt="">
+                        <img class="img-fluid mb-3 mb-sm-0" src="{{ asset('img/service-1.jpg') }}" alt="">
                     </div>
                     <div class="col-sm-7">
-                        <h4><i class="fa fa-coffee service-icon"></i>Service {{ $i }}</h4>
-                        <p class="m-0">Sit lorem ipsum et diam elitr est dolor sed duo...</p>
+                        <h4><i class="fa fa-truck service-icon"></i>Giao Hàng Tận Nơi</h4>
+                        <p class="m-0">Chúng tôi giao hàng nhanh chóng đến tận nhà, đảm bảo cà phê luôn tươi mới và giữ nguyên hương vị đặc trưng.</p>
                     </div>
                 </div>
             </div>
-            @endfor
+            <div class="col-lg-6 mb-5">
+                <div class="row align-items-center">
+                    <div class="col-sm-5">
+                        <img class="img-fluid mb-3 mb-sm-0" src="{{ asset('img/service-2.jpg') }}" alt="">
+                    </div>
+                    <div class="col-sm-7">
+                        <h4><i class="fa fa-coffee service-icon"></i>Cà Phê Rang Xay Tươi</h4>
+                        <p class="m-0">Hạt cà phê được rang xay mỗi ngày, mang đến hương vị đậm đà, thơm ngon nhất cho tách cà phê của bạn.</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-6 mb-5">
+                <div class="row align-items-center">
+                    <div class="col-sm-5">
+                        <img class="img-fluid mb-3 mb-sm-0" src="{{ asset('img/service-3.jpg') }}" alt="">
+                    </div>
+                    <div class="col-sm-7">
+                        <h4><i class="fa fa-award service-icon"></i>Chất Lượng Hàng Đầu</h4>
+                        <p class="m-0">Nguồn gốc rõ ràng, 100% hữu cơ, được chứng nhận chất lượng quốc tế. Cam kết mang đến sản phẩm tốt nhất.</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-6 mb-5">
+                <div class="row align-items-center">
+                    <div class="col-sm-5">
+                        <img class="img-fluid mb-3 mb-sm-0" src="{{ asset('img/service-4.jpg') }}" alt="">
+                    </div>
+                    <div class="col-sm-7">
+                        <h4><i class="fa fa-table service-icon"></i>Đặt Bàn Online</h4>
+                        <p class="m-0">Đặt bàn trước dễ dàng qua website, tiết kiệm thời gian và đảm bảo có chỗ ngồi ưng ý tại quán của chúng tôi.</p>
+                    </div>
+                </div>
+            </div>
         </div>
     </div>
 </div>
